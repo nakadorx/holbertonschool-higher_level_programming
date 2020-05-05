5de9e124a8b5762c5842aaf047d404c278ab139a
Nakadorx <nakadorx@gmail.com>
2020-05-05 15:34:02 +0200

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    la = len(tuple_a)
+    lb = len(tuple_b)
+    if la == 0:
+        a1 = 0
+        a2 = 0
+    elif la == 1:
+        a1 = tuple_a[0]
+        a2 = 0
+    else:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    if lb == 0:
+        b1 = 0
+        b2 = 0
+    elif lb == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    else:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+    new = (a1 + b1, a2 + b2)
+    return new
+
+
