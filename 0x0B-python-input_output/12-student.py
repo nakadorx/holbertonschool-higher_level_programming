e57e9014be35c6070bf6c02a5f1ab96cf34847a9
Nakadorx <nakadorx@gmail.com>
2020-07-15 16:21:49 +0100

    hgter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -6,13 +6,12 @@ class Student:
     """holbertontask"""
 
     def __init__(self, first_name, last_name, age):
-        """Initailazer"""
+        """holebron"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-
         if type(attrs) is list and all([type(x) == str for x in attrs]):
             return {key: value for key, value in self.__dict__.items() if key in attrs}
         return self.__dict__
