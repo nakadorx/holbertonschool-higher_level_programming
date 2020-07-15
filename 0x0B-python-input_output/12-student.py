05827d3b5d47d4a6ecbbf8cb5834343663ff0cdf
Nakadorx <nakadorx@gmail.com>
2020-07-15 16:23:25 +0100

    gergre

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -6,10 +6,12 @@ class Student:
     """holbertontask"""
 
     def __init__(self, first_name, last_name, age):
-        """holebron"""
+        """Initailazer"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
+
 
     def to_json(self, attrs=None):
         if type(attrs) is list and all([type(x) == str for x in attrs]):
--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -6,9 +6,10 @@ class Student:
 
     """holbertontask"""
     def __init__(self, first_name, last_name, age):
-		self.age = age
+        """Initailazer"""
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         dictionary = {}
