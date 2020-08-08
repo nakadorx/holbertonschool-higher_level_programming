81ed99bc2ba533765d61b6916ed9b1300ebcf784
nakadorx <nakadorx@gmail.com>
2020-08-08 15:31:35 +0200

    x

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 """
-accepts 3 arguments (mysql username, password and database name)
-and lists all cities from that database
+ con
 """
-import sys
 import MySQLdb
+import sys
 
 
+def mainx():
+    """con"""
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                INNER JOIN states\
+                ON cities.state_id = states.id\
+                ORDER BY cities.id ASC"
+                )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 4:
-    """connects to a given mysql database and lists all 'cities' from it"""
-		con = MySQLdb.connect(host="localhost", port=3306,
-							user=argv[1], passwd=argv[2], db=argv[3])
-		cur = con.cursor()
-		cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states\
-					ON cities.state_id = states.id ORDER BY cities.id ASC")
-		query_rows = cur.fetchall()
-		for row in query_rows:
-			print(row)
-		cur.close()
-		con.close()
-
+if __name__ == "__main__":
+    mainx()
