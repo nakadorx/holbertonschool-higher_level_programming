5a65fe28d2c16e379209e208f1fd573586d85bfc
nakadorx <nakadorx@gmail.com>
2020-08-08 16:22:04 +0200

    x

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 def mainx():
-""" con """
+    """ con """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -23,4 +23,3 @@ def mainx():
 
 if __name__ == '__main__':
     mainx()
-
