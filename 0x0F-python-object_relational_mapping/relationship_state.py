01939ed270c336829513d5075d78473ebb61304b
nakadorx <nakadorx@gmail.com>
2020-08-08 16:24:45 +0200

    x

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,11 +6,10 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from relationship_city import Base, City
 
-
 class State(Base):
     """ con"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True,
-                nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all,delete", backref='state')
+    cities = relationship("City",
+     cascade="all,delete", backref='state')
