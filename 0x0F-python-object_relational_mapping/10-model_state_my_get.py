ef5d82b75a258d8cdfb0f752ba5f0084e47d8ec6
nakadorx <nakadorx@gmail.com>
2020-08-08 16:12:48 +0200

    gergr

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,20 +7,21 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
 
-def mainx():
-""" con """
+def maix():
+    """con"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
     session = Session()
-    try:
-        instance = session.query(State).filter(State.name == sys.argv[4]).one()
-        print(str(instance.id))
-    except:
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+    if state:
+        print("{}".format(state.id))
+    else:
         print("Not found")
     session.close()
 
-
-if __name__ == '__main__':
-    mainx()
+if __name__ == "__main__":
+    maix()
