1710844ae8028a774c72d2cfdb36f4c1448b1b8b
nakadorx <nakadorx@gmail.com>
2020-08-08 16:17:54 +0200

    x

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,21 +7,20 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
 
-def maix():
-    """con"""
+def mainx():
+    """ con """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(name=sys.argv[4]).first()
-    if state:
-        print("{}".format(state.id))
-    else:
+    try:
+        instance = session.query(State).filter(State.name == sys.argv[4]).one()
+        print(str(instance.id))
+    except:
         print("Not found")
     session.close()
 
-if __name__ == "__main__":
-    maix()
+
+if __name__ == '__main__':
+    mainx()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -10,8 +10,7 @@ from relationship_city import Base, City
 class State(Base):
     """ con"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,\
-         unique=True, autoincrement=True,
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all,delete", backref='state')
