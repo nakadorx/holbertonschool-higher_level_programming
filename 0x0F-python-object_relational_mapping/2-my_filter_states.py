88ad2378a9e06a6d641c9c0b713c48cdb3b06542
nakadorx <nakadorx@gmail.com>
2020-08-08 15:26:31 +0200

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-conenciton
+ con
 """
-import sys
 import MySQLdb
+import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        """connects"""
-        con = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-        cur = con.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\
-                    ORDER BY id ASC".format(argv[4]))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        pass
-        cur.close()
-        con.close()
+def mainx():
+    """con"""
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cursor()
+    cur.execute("""SELECT *\
+                FROM states\
+                WHERE name LIKE BINARY '{}'\
+                ORDER BY id ASC""".format(sys.argv[4])
+                )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    mainx()
