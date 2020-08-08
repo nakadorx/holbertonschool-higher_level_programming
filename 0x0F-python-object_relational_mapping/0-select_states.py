4bfcf518c9f6bd101e8b4700409b17a232efc6db
nakadorx <nakadorx@gmail.com>
2020-08-08 15:15:29 +0200

    gertg

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,10 @@
-#!/usr/bin/python3
-"""
-con
-"""
-import MySQLdb
+ #!/usr/bin/python3
+# con
 import sys
-
-
-def main():
-    """con"""
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    [print(state) for state in cur.fetchall()]
