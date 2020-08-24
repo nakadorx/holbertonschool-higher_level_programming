c4c582fcdd5535ae78e442671a6ce7c9ec0d0623
nakadorx <nakadorx@gmail.com>
2020-08-24 14:42:04 +0200

    ger

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-reqq = urllib.request.Request(url)
-with urllib.request.urlopen(reqq) as response:
-    print(dict(response.headers).get("X-Request-Id"))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    reqq = urllib.request.Request(url)
+    with urllib.request.urlopen(reqq) as response:
+        print(dict(response.headers).get("X-Request-Id"))
