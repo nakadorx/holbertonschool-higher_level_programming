a5c23dbec3ca640560c847e6ce7ecf8ba323fa39
nakadorx <nakadorx@gmail.com>
2020-09-21 18:28:38 +0200

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+const Sq = require('./5-square');
+class Square extends Sq {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log((c).repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
