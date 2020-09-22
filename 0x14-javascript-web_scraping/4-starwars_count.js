c3405acd912263f2d6dfcbd61cb0ef47c0c93c41
nakadorx <nakadorx@gmail.com>
2020-09-23 01:08:52 +0200

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,6 @@
 const req = require('request');
 let res = [];
 req.get(process.argv[2], (error, respponse, body) => {
-
   if (error) {
     console.log(error);
   } else {
@@ -13,7 +12,6 @@ req.get(process.argv[2], (error, respponse, body) => {
   for (const m in res) {
     const chars = res[m].characters;
     for (const char in chars) {
-
       if (chars[char].endsWith('/18/')) {
         sum += 1;
       }
